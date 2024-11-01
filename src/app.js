c5d05a1254a5cc43ceac37ae35a2a9cca496bf07
Kamal Ghimire <kamalghimire121@gmail.com>
2024-11-01 14:55:15 +0000

    Sync ArgoCD Application

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8082;
 
 app.get('/', (req, res) => {
-    res.send('Hello World! I am lexmark V3');
+    res.send('Hello World! I am lexmark V5');
 });
 
 app.listen(port, () => {
